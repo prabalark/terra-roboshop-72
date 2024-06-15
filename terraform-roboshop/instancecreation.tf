6b29d1951103f128aff4207e4ba85ab3d84a61fc
prabala <radhakrishnaprabala8@gmail.com>
2024-06-15 17:59:07 +0530

    instancecreation.tf

---
+++ terraform-roboshop/instancecreation.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0b4f379183e5706b9" # devops-practice
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "web-server"
+  }
+}
+
+output "web" {
+  value = resource.aws_instance.web.public_ip
+}
