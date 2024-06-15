228e8cb4d6ee58d0aa7f91096143d53bf9f92d7e
prabala <radhakrishnaprabala8@gmail.com>
2024-06-15 18:05:05 +0530

    instancecreation.tf

--- terraform-roboshop/instancecreation.tf
+++ terraform-roboshop/instancecreation.tf
@@ -1,12 +1,5 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0b4f379183e5706b9" # devops-practice
-  instance_type = "t3.small"
-
-  tags = {
-    Name = "web-server"
-  }
-}
-
-output "web" {
-  value = resource.aws_instance.web.public_ip
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
 }
