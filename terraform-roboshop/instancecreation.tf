795d3d118a285726eef156ace7b4e1f7ceecfb39
prabala <radhakrishnaprabala8@gmail.com>
2024-06-15 18:31:57 +0530

    instancecreation.tf-ver1

--- terraform-roboshop/instancecreation.tf
+++ terraform-roboshop/instancecreation.tf
@@ -6,7 +6,7 @@ data "aws_ami" "ami" {
 
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.ami.image_id # devops-practice
-  instance_type = "t3.small"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "frontend"
@@ -15,52 +15,52 @@ resource "aws_instance" "frontend" {
 
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.ami.image_id # devops-practice
-  instance_type = "t3.small"
+  instance_type = "t3.medium"
 
   tags = {
-    Name = "frontend"
+    Name = "mongodb"
   }
 }
 
 resource "aws_instance" "catalogue" {
   ami           = data.aws_ami.ami.image_id # devops-practice
-  instance_type = "t3.small"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "frontend"
+    Name = "catalogue"
   }
 }
 
 resource "aws_instance" "redis" {
   ami           = data.aws_ami.ami.image_id # devops-practice
-  instance_type = "t3.small"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "frontend"
+    Name = "redis"
   }
 }
 
 resource "aws_instance" "user" {
   ami           = data.aws_ami.ami.image_id # devops-practice
-  instance_type = "t3.small"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "frontend"
+    Name = "user"
   }
 }
 
 resource "aws_instance" "cart" {
   ami           = data.aws_ami.ami.image_id # devops-practice
-  instance_type = "t3.small"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "frontend"
+    Name = "cart"
   }
 }
 
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.ami.image_id # devops-practice
-  instance_type = "t3.small"
+  instance_type = "t3.medium"
 
   tags = {
     Name = "frontend"
@@ -69,36 +69,36 @@ resource "aws_instance" "mysql" {
 
 resource "aws_instance" "shipping" {
   ami           = data.aws_ami.ami.image_id # devops-practice
-  instance_type = "t3.small"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "frontend"
+    Name = "shipping"
   }
 }
 
 resource "aws_instance" "rabbitmq" {
   ami           = data.aws_ami.ami.image_id # devops-practice
-  instance_type = "t3.small"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "frontend"
+    Name = "rabbitmq"
   }
 }
 
 resource "aws_instance" "payment" {
   ami           = data.aws_ami.ami.image_id # devops-practice
-  instance_type = "t3.small"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "frontend"
+    Name = "payment"
   }
 }
 
 resource "aws_instance" "dispatch" {
   ami           = data.aws_ami.ami.image_id # devops-practice
-  instance_type = "t3.small"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "frontend"
+    Name = "dispatch"
   }
 }
