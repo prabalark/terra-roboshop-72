62020aba223d896d90609f6d975c54364379c331
prabala <radhakrishnaprabala8@gmail.com>
2024-06-15 19:58:21 +0530

    sample.tf-ver1

--- learn terraform/sample.tf
+++ learn terraform/sample.tf
@@ -1,16 +1,3 @@
-## instance creation
-resource "aws_instance" "web" {
-  ami           = "ami-0b4f379183e5706b9" # devops-practice
-  instance_type = "t3.small"
-
-  tags = {
-    Name = "web-server"
-  }
-}
-output "web" {
-  value = aws_instance.web.public_ip
-}
-
 ## for dynamic pick up of ami
 data "aws_ami" "ami" {
   most_recent = true
@@ -18,8 +5,17 @@ data "aws_ami" "ami" {
   owners      = ["973714476881"]
 }
 
-output "ami" {
-  value = data.aws_ami.ami.image_id
+## instance creation
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ami.image_id # devops-practice
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "web-server"
+  }
+}
+output "web" {
+  value = aws_instance.web.public_ip
 }
 
 # create record
