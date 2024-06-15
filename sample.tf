521cb8c726e86b73c1322364d796eadc51354259
prabala <radhakrishnaprabala8@gmail.com>
2024-06-15 10:56:26 +0530

    sample.tf-check

--- sample.tf
+++ sample.tf
@@ -1,8 +1,4 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "web1" {
   ami           = "ami-0b4f379183e5706b9" # devops-practice
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "web-server"
   }
-}
