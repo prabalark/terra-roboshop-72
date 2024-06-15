fa3ceb95392391bd2d229c3031afbcd6a78e9bd6
prabala <radhakrishnaprabala8@gmail.com>
2024-06-15 18:25:43 +0530

    instancecreation.tf-ver1

--- terraform-roboshop/instancecreation.tf
+++ terraform-roboshop/instancecreation.tf
@@ -4,6 +4,101 @@ data "aws_ami" "ami" {
   owners      = ["973714476881"]
 }
 
- output "ami" {
-   value = data.aws_ami.ami.image_id
- }
+resource "aws_instance" "frontend" {
+  ami           = data.aws_ami.ami.image_id # devops-practice
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = data.aws_ami.ami.image_id # devops-practice
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = data.aws_ami.ami.image_id # devops-practice
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "redis" {
+  ami           = data.aws_ami.ami.image_id # devops-practice
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "user" {
+  ami           = data.aws_ami.ami.image_id # devops-practice
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "cart" {
+  ami           = data.aws_ami.ami.image_id # devops-practice
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = data.aws_ami.ami.image_id # devops-practice
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "shipping" {
+  ami           = data.aws_ami.ami.image_id # devops-practice
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = data.aws_ami.ami.image_id # devops-practice
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "payment" {
+  ami           = data.aws_ami.ami.image_id # devops-practice
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "dispatch" {
+  ami           = data.aws_ami.ami.image_id # devops-practice
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "frontend"
+  }
+}
