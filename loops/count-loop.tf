b68c10e26de053579683634635e6808147b1648c
prabala <radhakrishnaprabala8@gmail.com>
2024-06-20 14:04:08 +0530

    count-loop1.tf

--- loops/count-loop.tf
+++ loops/count-loop.tf
@@ -1,11 +1,1 @@
-resource "null_resource" "comp" {
-  count = length(var.comp)
-
-  provisioner "local-exec" {
-    command =  " echo comp_name = ${var.comp[count.index]} "
-  }
-}
-
-variable "comp" {
-  default = ["forntend","mangodb","catalogue"]
-}
+resource "null_resource" "null" {}
