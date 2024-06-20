9afc0c9a69fb783b6909538f0e551027990851a3
prabala <radhakrishnaprabala8@gmail.com>
2024-06-20 08:16:55 +0530

    count-loop.tf

--- loops/count-loop.tf
+++ loops/count-loop.tf
@@ -1,11 +1,11 @@
 resource "var_resource" "comp" {
      count = length(var.comp)
-     }
 
   ## provisioner is same like variable
-   provisioner  "local-exce" {
-     command = var.comp[count.index]
-   }
+  provisioner  "local-exce" {
+    command = var.comp[count.index]
+  }
+     }
 
  variable "comp" {
       default = ["forntend","mongodb","catalogue"]
