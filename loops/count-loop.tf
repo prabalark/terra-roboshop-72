fa85e9aed0393ebab8c5d473a82ed27b2c4a5859
prabala <radhakrishnaprabala8@gmail.com>
2024-06-20 17:53:43 +0530

    count-loop1.tf

--- loops/count-loop.tf
+++ loops/count-loop.tf
@@ -2,7 +2,7 @@ resource "null_resource" "comp8" {
   for_each = var.comp
 
   provisioner "local-exec" {
-    command = "echo  fruit_name --> ${each.key} => ${each.value["fr_n"]} = ${each.value["price"]}"
+    command = "echo  fruit_name => ${each.value["fr_n"]} = ${each.value["price"]}"
   }
 }
 
