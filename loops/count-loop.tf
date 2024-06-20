479b3535e394d237484fe5b01d8d869e8e00db87
prabala <radhakrishnaprabala8@gmail.com>
2024-06-20 09:16:19 +0530

    count-loop1.tf

--- loops/count-loop.tf
+++ loops/count-loop.tf
@@ -1,16 +1,1 @@
-resource "null_resource" "comp" {
-  count = length(var.comp)
-
-  ## provisioner is same like variable
-  provisioner  "local-exce" {
-    command = var.comp[count.index]
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-variable "comp" {
-  default = ["forntend","mongodb","catalogue"]
-}
+resource "null_resource" "null" {}
