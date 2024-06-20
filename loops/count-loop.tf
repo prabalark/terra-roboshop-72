086d36bd41acf8ff0ef92e2a8cb54168a76c91dd
prabala <radhakrishnaprabala8@gmail.com>
2024-06-20 07:27:53 +0530

    count-loop.tf

--- loops/count-loop.tf
+++ loops/count-loop.tf
@@ -1,6 +1,10 @@
-resource "null_resource" "null" {
+resource "var_resource" "comp" {
      count = length(var.comp)
      }
+
+ provisioner "var_res" {
+      command = var_resource[count.index]
+ }
 
  variable "comp" {
       default = ["forntend","mongodb","catalogue"]
