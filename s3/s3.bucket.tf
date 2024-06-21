5fb23976717862f2bf33d270110b50900ac7c2d5
prabala <radhakrishnaprabala8@gmail.com>
2024-06-21 23:11:01 +0530

    s3.bucket.tf

--- s3/s3.bucket.tf
+++ s3/s3.bucket.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-72-rk"
-    key    = "roboshop/devenv/terraform.tfstate"
-    region = "us-east-1"
-  }
+    bucket = "terraform-72-rk"                   # aws --> s3
+    key    = "roboshop/devenv/terraform.tfstate" # give any path ( give folder )+ .terraform.tfstate [ is very imp file,if we loose file all data will gone]
+    region = "us-east-1"   #depend on usage
 }
