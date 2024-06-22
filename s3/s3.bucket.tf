87f308430a0c4303da7908222d3815b159e49754
prabala <radhakrishnaprabala8@gmail.com>
2024-06-22 20:10:22 +0530

    s3.bucket.tf-prd

--- s3/s3.bucket.tf
+++ s3/s3.bucket.tf
@@ -1,7 +1,7 @@
   terraform {
     backend "s3" {
       bucket = "terraform-72-rk"
-      key    = "roboshop/devenv/terraform.tfstate"
+      key    = "roboshop/prodenv/terraform.tfstate"
       region = "us-east-1"
     }
   }
