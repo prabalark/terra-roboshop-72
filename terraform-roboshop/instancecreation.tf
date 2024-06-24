8fcae7ea3dd2b3435e1dcb21bce6a302288c34c2
prabala <radhakrishnaprabala8@gmail.com>
2024-06-24 17:13:46 +0530

    instancecreation.tfvars-ver1

--- terraform-roboshop/instancecreation.tf
+++ terraform-roboshop/instancecreation.tf
@@ -30,38 +30,38 @@ variable "component" {
       Name ="catalogue"
       instance_type = "t3.micro"
     }
-    Redis ={
-      Name ="Redis"
-      instance_type = "t3.small"
-    }
-    User ={
-      Name ="User"
-      instance_type = "t3.micro"
-    }
-    Cart ={
-      Name ="Cart"
-      instance_type = "t3.micro"
-    }
-    MySQL ={
-      Name ="MySQL"
-      instance_type = "t3.small"
-    }
-    Shipping ={
-      Name ="Shipping"
-      instance_type = "t3.medium"
-    }
-    RabbitMQ ={
-      Name ="RabbitMQ"
-      instance_type = "t3.small"
-    }
-    Payment ={
-      Name ="Payment"
-      instance_type = "t3.small"
-    }
-    Dispatch ={
-      Name ="Dispatch"
-      instance_type = "t3.small"
-    }
+#    Redis ={
+#      Name ="Redis"
+#      instance_type = "t3.small"
+#    }
+#    User ={
+#      Name ="User"
+#      instance_type = "t3.micro"
+#    }
+#    Cart ={
+#      Name ="Cart"
+#      instance_type = "t3.micro"
+#    }
+#    MySQL ={
+#      Name ="MySQL"
+#      instance_type = "t3.small"
+#    }
+#    Shipping ={
+#      Name ="Shipping"
+#      instance_type = "t3.medium"
+#    }
+#    RabbitMQ ={
+#      Name ="RabbitMQ"
+#      instance_type = "t3.small"
+#    }
+#    Payment ={
+#      Name ="Payment"
+#      instance_type = "t3.small"
+#    }
+#    Dispatch ={
+#      Name ="Dispatch"
+#      instance_type = "t3.small"
+#    }
   }
 }
 
