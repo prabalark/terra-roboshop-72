40b8fc77726d55e4fd71d692840d2a20d3362893
prabala <radhakrishnaprabala8@gmail.com>
2024-06-24 23:43:14 +0530

    prod-var.tfvars-ver1

--- terraform-roboshop/resouce.tf
+++ terraform-roboshop/resouce.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "instance" {
-  for_each = var.component
+  for_each = var.components
   ami           = data.aws_ami.ami.image_id # devops-practice
   instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.security_group.id]
@@ -11,7 +11,7 @@ resource "aws_instance" "instance" {
 
 # create record
 resource "aws_route53_record" "records" {
-  for_each = var.component
+  for_each = var.components
   zone_id = "Z03484922ZJTTIW47BAFL"
   name    = "${each.value["Name"]}.devops72bat.online"
   type    = "A"
