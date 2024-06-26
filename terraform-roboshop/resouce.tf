7e1a446a41816d5367002e2582e7ab3ceed0dbbc
prabala <radhakrishnaprabala8@gmail.com>
2024-06-26 10:09:47 +0530

    resource.tf-ver3

--- terraform-roboshop/resouce.tf
+++ terraform-roboshop/resouce.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "instance" {
 }
 
   # create shell
-resource "null_resource" "provisioner1" {
+resource "null_resource" "provisioner2" {
 
   depends_on = [aws_instance.instance, aws_route53_record.records]
 
