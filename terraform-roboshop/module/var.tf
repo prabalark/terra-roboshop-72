2748514b28868de081593fa2b161f79f92267857
prabala <radhakrishnaprabala8@gmail.com>
2024-07-30 22:32:38 +0530

    var.tf vr2

--- terraform-roboshop/module/var.tf
+++ terraform-roboshop/module/var.tf
@@ -1,8 +1,8 @@
-variable "env" {}            #modvar
-variable "compenent_name" {} #modvar
-variable "instance_type" {}  #modvar
-variable "password" {}       #modvar
-variable "provisioner" {     #modvar
+variable "env" {}
+variable "compenent_name" {}
+variable "instance_type" {}
+variable "password" {}
+variable "provisioner" {
   default = false
 }
 
