deab274962967c00552d22a6af952dbd8708d055
prabala <radhakrishnaprabala8@gmail.com>
2024-08-02 09:36:57 +0530

    resouce.tf vr4

--- terraform-roboshop/module/resouce.tf
+++ terraform-roboshop/module/resouce.tf
@@ -43,3 +43,25 @@ resource "aws_route53_record" "records" {
   records = [aws_instance.instance.private_ip]  # single instance
 }
 
+resource "aws_iam_role" "test_role" {
+  name = "${var.compenent_name}-${var.env}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com" # this is imp
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${var.compenent_name}-${var.env}-role"
+  }
+}
+
