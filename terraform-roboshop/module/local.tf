f9d7ca2f5cd87cfe93dabf6969e58ffa12d74e7f
prabala <radhakrishnaprabala8@gmail.com>
2024-08-06 17:04:45 +0530

    loca.tf vr1

--- terraform-roboshop/module/local.tf
+++ terraform-roboshop/module/local.tf
@@ -1,18 +1,18 @@
-locals {
-  name = var.env !="" ? "${var.compenent_name}-${var.env}" : var.compenent_name
-
-   db_commands =[
-     "rm -rf terraform-roboshop",
-     "git clone https://github.com/prabalark/roboshop-72.git",
-     "cd roboshop-72",
-     "sudo bash ${var.compenent_name}.sh  ${var.password} "
-   ]
-
-    app_commands = [
-     // "sudo labauto ansible" ,
-     // "ansible-pull -i localhost, -U https://github.com/prabalark/roboshop-ansible-72.git roboshop-ani.yml -e role_name=${var.compenent_name} -e env=${var.env}",
-      "echo ok"
-    ]
-
-}
+#locals {
+#  name = var.env !="" ? "${var.compenent_name}-${var.env}" : var.compenent_name
+#
+#   db_commands =[
+#     "rm -rf terraform-roboshop",
+#     "git clone https://github.com/prabalark/roboshop-72.git",
+#     "cd roboshop-72",
+#     "sudo bash ${var.compenent_name}.sh  ${var.password} "
+#   ]
+#
+#    app_commands = [
+#     // "sudo labauto ansible" ,
+#     // "ansible-pull -i localhost, -U https://github.com/prabalark/roboshop-ansible-72.git roboshop-ani.yml -e role_name=${var.compenent_name} -e env=${var.env}",
+#      "echo ok"
+#    ]
+#
+#}
 
