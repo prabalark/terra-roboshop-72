18fa24f56cfb7ec7e03ed08dae5d472f14bcb5a1
prabala <radhakrishnaprabala8@gmail.com>
2024-08-10 11:51:31 +0530

    local.tf vr2

--- terraform-roboshop/module/local.tf
+++ terraform-roboshop/module/local.tf
@@ -9,9 +9,8 @@ locals {
    ]
 
     app_commands = [
-     // "sudo labauto ansible" ,
-     // "ansible-pull -i localhost, -U https://github.com/prabalark/roboshop-ansible-72.git roboshop-ani.yml -e role_name=${var.compenent_name} -e env=${var.env}",
-      "echo ok"
+      "sudo labauto ansible" ,
+      "ansible-pull -i localhost, -U https://github.com/prabalark/roboshop-ansible-72.git roboshop-ani.yml -e role_name=${var.compenent_name} -e env=${var.env}"
     ]
 
 }
