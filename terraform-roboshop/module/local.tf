a3dec0b2a9fa757073b713a477f704bed7dafcc2
prabala <radhakrishnaprabala8@gmail.com>
2024-08-27 23:17:02 +0530

    local.tf vr6

--- terraform-roboshop/module/local.tf
+++ terraform-roboshop/module/local.tf
@@ -13,10 +13,10 @@ locals {
       "ansible-pull -i localhost, -U https://github.com/prabalark/roboshop-ansible-72.git roboshop-ani.yml -e role_name=${var.compenent_name} -e env=${var.env}"
     ]
     db_tags = {
-       name = "${var.compenent_name}-${var.env}"
+       Name = "${var.compenent_name}-${var.env}"
     }
     app_tags = {
-       name = "${var.compenent_name}-${var.env}"
+       Name = "${var.compenent_name}-${var.env}"
        monitor = "true"
        component = var.compenent_name
        env = var.env
