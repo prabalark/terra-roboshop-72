4e7846fddad7873dfc639aa83f8bf47b3d4d9ac7
prabala <radhakrishnaprabala8@gmail.com>
2024-08-28 21:57:37 +0530

    local.tf vr5

--- terraform-roboshop/module/local.tf
+++ terraform-roboshop/module/local.tf
@@ -18,7 +18,7 @@ locals {
     app_tags = {
        Name = "${var.compenent_name}-${var.env}"
        monitor = "true"
-       component = var.compenent_name
+       compenent_name = var.compenent_name
        env = var.env
     }
 }
