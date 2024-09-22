0fd944032b9e0c9b60f4a0709f1a4456d4779368
prabala <radhakrishnaprabala8@gmail.com>
2024-09-22 15:09:21 +0530

    var.tf

--- terraform-roboshop/module/var.tf
+++
@@ -1,9 +0,0 @@
-variable "env" {}
-variable "compenent_name" {}
-variable "instance_type" {}
-variable "password" {}
-variable "provisioner" {
-  default = false
-}
-variable "app_type" {} # [ only in local.tf ]
-
--- terraform-roboshop/var.tf
+++ terraform-roboshop/var.tf
@@ -1,4 +1,1 @@
 variable "env" {}          # to identify the dev (or) prod
-variable "database_server" {}   # dynamic variable
-variable "app_server" {}   # dynamic variable
-# variable "component" {}   # dynamic variable, into to 2 parts ie - database_server,app_server
