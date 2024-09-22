55cb4e5e2b3589ad2716e74c06806bd5bdd29c4d
prabala <radhakrishnaprabala8@gmail.com>
2024-09-22 16:49:35 +0530

    main.tfvars vr1

--- terraform-roboshop/tf-module-vpc/main.tf
+++ terraform-roboshop/tf-module-vpc/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block  # var1 keep in envdev-var.tfvars
+  cidr_block = var.cidr_block  # var1 keep in envdev-main.tfvars
   enable_dns_support = true
   enable_dns_hostnames = true
 }
